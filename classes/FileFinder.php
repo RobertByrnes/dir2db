cf2d71ec8f0d457d276254667c0c5097143164aa
Robert Byrnes <robbyrnes@hotmail.co.uk>
2021-04-16 21:34:36 +0100

    initial push

---
+++ classes/FileFinder.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Trait FileFinder uses PHP's recursive iterator classes to search for PHP files in a directory
+ * structure returning an array of results.
+ */
+Trait FileFinder
+{
+    /**
+     * Directory names to exclude from the search.
+     * 
+     * @var string
+     */
+    public string $directoryFilter = '/smarty|vendor|node_modules|deepscripts|blackeye|phpBrute|phpChart_light|parallel/i';
+
+    /**
+     * Regex to find PHP file extension.
+     * 
+     * @var string
+     */
+    public string $fileFilter = '/\.(?:php)$/';
+
+    /**
+     * The callable function of Trait FileFinder, calls $this recursiveRegexIterator.
+     *
+     * @param string $path
+     * @param string $fileFilter
+     * @param string $directoryFilter
+     * @return array
+     */
+    public function fileFinder(string $path, string $fileFilter=NULL, string $directoryFilter=NULL) : array
+    {
+        if (!is_null($fileFilter))
+        {
+            $this->fileFilter = $fileFilter;
+        }
+        if (!is_null($directoryFilter))
+        {
+            $this->directoryFilter = $directoryFilter;
+        }
+        return $this->recursiveRegexIterator($path);
+    }
+
+    /**
+     * Iteratives recursively through directories using regex to filter file results.
+     *
+     * @param string $path
+     * @return array
+     */
+    public function recursiveRegexIterator(string $path) : array
+    {
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+        $phpFile = new RegexIterator($iterator, $this->fileFilter);
+        
+        $files = array();
+        
+        foreach ($phpFile as $info)
+        {
+            if(!preg_match($this->directoryFilter, $info))
+            {
+                $files[] = $info->getPathname();
+            }
+        }
+        if (!empty($files))
+        {
+            return $files;
+        }
+        else
+        {
+            $files[] = 'No files discovered within search parameters.';
+            return $files;
+        }
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3fb33001213e6837745a285636e2798038b7e1cc',
+    'name' => 'robertbyrnes/directorytodatabase',
+  ),
+  'versions' => 
+  array (
+    'robertbyrnes/dataconnection' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '40385ff057180540b9e194f1b2a0d4a9054fb6a2',
+    ),
+    'robertbyrnes/directorytodatabase' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3fb33001213e6837745a285636e2798038b7e1cc',
+    ),
+  ),
+);
