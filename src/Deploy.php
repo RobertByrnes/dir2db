4ebec8aedec48317317419a0ebd811e4fd0aea8a
Robert Byrnes <robert.byrnes@exe-squared.co.uk>
2023-03-29 08:33:12 +0100

    fix: update directory locations

--- src/Deploy.php
+++ src/Deploy.php
@@ -9,12 +9,12 @@ class Deploy
 {
     public static function postPackageInstall($event)
     {   
-        if (!is_dir('../private') && !mkdir('../private') && !is_dir('../private')) {
+        if (!is_dir(__DIR__.'/../private') && !mkdir(__DIR__.'/../private') && !is_dir(__DIR__.'/../private')) {
             throw new \RuntimeException(sprintf('Directory "%s" was not created', '../private'));
         }
 
         if (!file_exists('../private/local.ini')) {
-            copy('src/local.ini.example', 'private/local.ini');
+            copy(__DIR__.'/local.ini.example', __DIR__.'/../private/local.ini');
         }
     }
 }
