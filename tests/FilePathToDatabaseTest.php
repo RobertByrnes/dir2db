24a037c9e721e4ca00065c7407c92c4916e11fdb
RobertByrnes <robbyrnes@hotmail.co.uk>
2023-03-30 20:52:41 +0100

    test: edit test to use DIRECTORY_SEPARATOR

--- tests/FilePathToDatabaseTest.php
+++ tests/FilePathToDatabaseTest.php
@@ -11,14 +11,9 @@ final class FilePathToDatabaseTest extends TestCase
     use FileFinder;
 
     private array $argv;
-    private string $dirSeparator = '/';
 
     public function setUp(): void
     {
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $this->dirSeparator = '\\';
-        }
-
         $this->argv = explode(',', $_SERVER['argv']);
     }
 
@@ -29,10 +24,10 @@ final class FilePathToDatabaseTest extends TestCase
         $testExclusions = $this->argv[2];
 
         $expected = [
-            $testPath.$this->dirSeparator.'dir2db.php',
-            $testPath.$this->dirSeparator.'Environment.php',
-            $testPath.$this->dirSeparator.'FileFinder.php',
-            $testPath.$this->dirSeparator.'FilePathToDatabase.php'
+            $testPath.DIRECTORY_SEPARATOR.'dir2db.php',
+            $testPath.DIRECTORY_SEPARATOR.'Environment.php',
+            $testPath.DIRECTORY_SEPARATOR.'FileFinder.php',
+            $testPath.DIRECTORY_SEPARATOR.'FilePathToDatabase.php'
         ];
 
         $expected = $this->lowerCaseFilePaths($expected);
@@ -50,7 +45,7 @@ final class FilePathToDatabaseTest extends TestCase
         $testExclusions = $this->argv[2];
 
         $expected = [
-            $testPath.$this->dirSeparator.'dir2db.sql'
+            $testPath.DIRECTORY_SEPARATOR.'dir2db.sql'
         ];
 
         $files = $this->fileFinder($testPath, $testRegex, $testExclusions);
@@ -67,12 +62,12 @@ final class FilePathToDatabaseTest extends TestCase
         $testExclusions = $this->argv[2];
 
         $expected = [
-            $testPath.$this->dirSeparator.'dir2db.php',
-            $testPath.$this->dirSeparator.'dir2db.sql',
-            $testPath.$this->dirSeparator.'Environment.php',
-            $testPath.$this->dirSeparator.'FileFinder.php',
-            $testPath.$this->dirSeparator.'FilePathToDatabase.php',
-            $testPath.$this->dirSeparator.'local.ini.example'
+            $testPath.DIRECTORY_SEPARATOR.'dir2db.php',
+            $testPath.DIRECTORY_SEPARATOR.'dir2db.sql',
+            $testPath.DIRECTORY_SEPARATOR.'Environment.php',
+            $testPath.DIRECTORY_SEPARATOR.'FileFinder.php',
+            $testPath.DIRECTORY_SEPARATOR.'FilePathToDatabase.php',
+            $testPath.DIRECTORY_SEPARATOR.'local.ini.example'
         ];
 
         $expected = $this->lowerCaseFilePaths($expected);
@@ -89,11 +84,11 @@ final class FilePathToDatabaseTest extends TestCase
         $testRegex = $this->argv[1];    
         
         $expected = [
-            $testPath.$this->dirSeparator.'dir2db.php',
-            $testPath.$this->dirSeparator.'Environment.php',
-            $testPath.$this->dirSeparator.'FileFinder.php',
-            $testPath.$this->dirSeparator.'FilePathToDatabase.php',
-            $testPath.$this->dirSeparator.'vendor/dir2db.php'
+            $testPath.DIRECTORY_SEPARATOR.'dir2db.php',
+            $testPath.DIRECTORY_SEPARATOR.'Environment.php',
+            $testPath.DIRECTORY_SEPARATOR.'FileFinder.php',
+            $testPath.DIRECTORY_SEPARATOR.'FilePathToDatabase.php',
+            $testPath.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'dir2db.php'
         ];     
         
         $expected = $this->lowerCaseFilePaths($expected);
---
+++ tests/testdir/vendor/dir2db.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace dir2db;
+
+require_once(__DIR__.'/../vendor/autoload.php');
+
+print("
+                        ______     _             _____    ______   ______     
+                       |_   _ `.  (_)           / ___ `. |_   _ `.|_   _ \    
+                         | | `. \ __   _ .--.  |_/___) |   | | `. \ | |_) |   
+                         | |  | |[  | [ `/''\]  .'.....'   | |  | | |  __'.   
+                        _| |_.' / | |  | |     / /_____   _| |_.' /_| |__) |  
+                       |______.' [___][___]    |_______| |______.'|_______/   
+                                                                            
+");
+print("
+                                            .----.______
+                                            | v1.0.8    |
+                                            |    ___________
+                                            |   /          /
+                                            |  /          /
+                                            | /          /
+                                            |/__________/
+
+");
+
+$shortopts = "p:r:e::h";
+
+$longopts = array(
+    "path:",
+    "regex:",
+    "exclusions::",
+    "help"
+);
+
+$options = getopt($shortopts, $longopts);
+
+switch (TRUE) {
+    case isset($options['p']): $path = $options['p']; break;
+    case isset($options['h']): help(); break;
+}
+
+(empty($options['e'])) ? $exludeDirs = null : $exludeDirs = $options['e'];
+(empty($options['r'])) ? $regex = null : $regex = $options['r'];
+(!empty($path)) ? run($path, $exludeDirs, $regex) : help();
+
+function run($path, $exludeDirs=null, $regex=null) : void
+{
+    $program = new FilePathToDatabase($path, $exludeDirs, $regex);
+}
+
+function help()
+{
+    $helpMessage = "\n
+    /*** ARGUMENTS ***/\n
+    Required arguments:
+        -p path, e.g. c:/wamp/www/
+    Optional arguments: 
+        -r regex, to filter file search to only include certain file extension. Default: \"/\.(?:php)$/\"
+        -e exclusions, directories ommited during search e.g. vendor|node_modules|private *Must include pipe*
+        -h help, prints this help message.";
+    print("\n".$helpMessage."\n");
+    print("\n    Dir2DB, written by Robert Byrnes,
+    under GPLv3 licence. https://github/RobertByrnes/Directory_to_Database\n");
+    die();
+}
